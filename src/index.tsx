d50fc570a6ac7bb67fe47be94711437eeeb69a46
Anton Nakonechny <anakonechny@alfabank.ru>
2023-11-18 04:04:43 +0100

    feat(*): сборка через ts loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { App } from '@/components/app';
+import { App } from '~/components/app';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const root = document.getElementById('root');
