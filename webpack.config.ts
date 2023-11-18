b06a02c9e09cf2819e9d9bab94ed45a97cdf0d96
Anton Nakonechny <anakonechny@alfabank.ru>
2023-11-18 06:50:42 +0100

    fix(*): поправить правила prettier

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,7 @@
-import path from "path";
-import webpack from "webpack";
-import { buildWebpack } from "./config/build/buildWebpack";
-import {
-  BuildMode,
-  BuildPaths,
-  BuildPlatform,
-} from "./config/build/types/types";
+import path from 'path';
+import webpack from 'webpack';
+import { buildWebpack } from './config/build/buildWebpack';
+import { BuildMode, BuildPaths, BuildPlatform } from './config/build/types/types';
 
 interface EnvVariables {
   mode?: BuildMode;
@@ -16,19 +12,19 @@ interface EnvVariables {
 
 export default (env: EnvVariables) => {
   const paths: BuildPaths = {
-    output: path.resolve(__dirname, "build"),
-    entry: path.resolve(__dirname, "src", "index.tsx"),
-    html: path.resolve(__dirname, "public", "index.html"),
-    public: path.resolve(__dirname, "public"),
-    src: path.resolve(__dirname, "src"),
+    output: path.resolve(__dirname, 'build'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    public: path.resolve(__dirname, 'public'),
+    src: path.resolve(__dirname, 'src'),
   };
 
   const config: webpack.Configuration = buildWebpack({
     port: env.port ?? 3000,
-    mode: env.mode ?? "development",
+    mode: env.mode ?? 'development',
     paths,
     analyzer: env.analyzer,
-    platform: env.platform ?? "desktop",
+    platform: env.platform ?? 'desktop',
   });
 
   return config;
