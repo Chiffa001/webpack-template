b96f3b761cfcef4a43469d9f2a7dd5afe5903bab
Anton Nakonechny <anakonechny@alfabank.ru>
2023-11-18 03:58:38 +0100

    feat(*): удалить ненужные файлы

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,30 @@
-import {createRoot} from "react-dom/client";
-import {App} from "./components/App/App";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {LazyAbout} from "@/pages/about/About.lazy";
-import {Shop} from "@/pages/Shop";
-import {Suspense} from "react";
+import { createRoot } from 'react-dom/client';
+import { App } from '@/components/app';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
-if(!root) {
-    throw new Error('root not found')
+if (!root) {
+  throw new Error('root not found');
 }
 
-const container = createRoot(root)
+const container = createRoot(root);
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                path: '/about',
-                element:  <Suspense fallback={'Loading...'}><LazyAbout /></Suspense>
-            },
-            {
-                path: '/shop',
-                element: <Suspense fallback={'Loading...'}><Shop /></Suspense>
-            },
-        ]
-    },
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      //   {
+      //     path: '/path',
+      //     element: (
+      //       <Suspense fallback={'Loading...'}>
+      //         <LazyComponent />
+      //       </Suspense>
+      //     ),
+      //   },
+    ],
+  },
 ]);
 
-container.render(
-    <RouterProvider router={router} />
-)
+container.render(<RouterProvider router={router} />);
